65db42464aaa521857ddfea0e051027ce12d3020
thlorenz <thlorenz@8eca4967-6344-411a-a426-5af860f6c464>
2009-10-14 21:34:42 +0000

    git-svn-id: http://svn.xp-dev.com/svn/Software@5 8eca4967-6344-411a-a426-5af860f6c464

---
+++ PokerTell/trunk/Src/PokerTell.PokerHand/Views/HandHistoriesView.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace PokerTell.PokerHand.Views
+{
+    using System.Windows.Controls;
+
+    using Infrastructure.Interfaces.PokerHand;
+
+    /// <summary>
+    /// Interaction logic for HandHistoriesView.xaml
+    /// </summary>
+    public partial class HandHistoriesView : UserControl
+    {
+        public HandHistoriesView()
+        {
+            InitializeComponent();
+        }
+
+        public HandHistoriesView(IHandHistoriesViewModel viewModel)
+            : this()
+        {
+            DataContext = viewModel;
+        }
+    }
+}
