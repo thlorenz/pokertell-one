4c1efcebc053a85f3df843e39c39ab7f85f2edc0
thlorenz <thlorenz@gmx.de>
2010-03-08 10:07:43 -0500

    Created WindowManipulator and interface

--- Src/Libraries/Tools/WPF/Interfaces/IWindowManager.cs
+++ Src/Libraries/Tools/WPF/Interfaces/IWindowManager.cs
@@ -17,5 +17,13 @@ namespace Tools.WPF.Interfaces
         IWindowManager Hide();
 
         IntPtr Handle { get; }
+
+        double Left { get; set; }
+
+        double Top { get; set; }
+
+        double Width { get; set; }
+
+        double Height { get; set; }
     }
 }
