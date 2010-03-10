01b099a0dfd5178497610eafa553af71553a0ff0
thlorenz <thlorenz@gmx.de>
2010-03-10 08:15:36 -0500

    Fixed the _runalltests.bat by removing all options from nunit runner - otherwise it threw exceptions. Now MSpec takes about 10s for 434 specs and NUnit 48s for 1254 tests -> still under 1min total.

---
+++ Build.Tests/runalltests.bat
@@ -0,0 +1,2 @@
+"C:\Program Files\Mspec\mspec" PokerTell.Statistics.Tests.dll PokerTell.LiveTracker.Tests.dll -t --html ".\Mspec.Tests.Report.html"
+"C:\Program Files\TestDriven.NET 2.0\NUnit\2.5\net-2.0\nunit-console" PokerTell.DatabaseSetup.Tests.dll PokerTell.LiveTracker.Tests.dll PokerTell.PokerHand.Tests.dll PokerTell.PokerHandParsers.Tests.dll PokerTell.Repository.Tests.dll PokerTell.Statistics.Tests.dll PokerTell.User.Tests.dll Tools.Tests.dll 
